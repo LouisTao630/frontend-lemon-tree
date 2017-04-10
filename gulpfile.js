3c7e42bc0a6311304dbd77aedda3e0507ebf0a43
Louis Tao <louistao630@sina.com>
2017-04-11 00:47:21 +0800

    apply babelify browserify module

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var gulp =require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
 gulp.task('default',function(){
 	console.log('I am about to learn the essentials of React.js');
 })
